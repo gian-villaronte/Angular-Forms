7a6ac909f1bb16400b4de8b1392f8a9f8e5a540d
Gian Villaronte <gianvillaronte@gmail.com>
2023-11-09 14:54:19 -0500

    Angular Forms
    Template Driven and Reactive

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,16 +1,30 @@
+import { HttpClientModule } from '@angular/common/http';
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { CartComponent } from './cart/cart.component';
+import { ProductsModule } from './products/products.module';
+import { PageNotFoundComponent } from './page-not-found/page-not-found.component';
+import { AuthModule } from './auth/auth.module';
+import { CommonModule } from '@angular/common';
+import { ReactiveFormsModule } from '@angular/forms';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    CartComponent,
+    PageNotFoundComponent
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    ProductsModule,
+    AppRoutingModule,
+    HttpClientModule,
+    AuthModule,
+    ReactiveFormsModule,
+    CommonModule
   ],
   providers: [],
   bootstrap: [AppComponent]
---
+++ src/app/products/product-create/product-create.component.ts
@@ -0,0 +1,44 @@
+import { Component, OnInit, EventEmitter, Output } from '@angular/core';
+import { Product } from '../product';
+import { ProductsService } from '../products.service';
+import { FormControl, FormGroup, Validators } from '@angular/forms';
+import { priceRangeValidator } from '../pricerange.directive';
+
+
+@Component({
+  selector: 'app-product-create',
+  templateUrl: './product-create.component.html',
+  styleUrls: ['./product-create.component.css']
+})
+export class ProductCreateComponent implements OnInit{
+
+  get name() { return this.productForm.controls.name }
+  get price() { return this.productForm.controls.price }
+  showPriceRangeHint = false;
+
+  @Output() added = new EventEmitter<Product>();
+
+  constructor(private productsService: ProductsService) {}
+
+  createProduct() {
+    this.productsService.addProduct(this.name.value, Number(this.
+      price.value)).subscribe(product => {
+      this.productForm.reset();
+      this.added.emit(product);
+    });
+  }
+
+  productForm = new FormGroup({
+    name: new FormControl('', { nonNullable: true, validators: Validators.required }),
+    price: new FormControl<number | undefined>(undefined, {
+   nonNullable: true, validators: [Validators.required, priceRangeValidator()] })
+   });
+
+   ngOnInit(): void {
+    this.price.valueChanges.subscribe(price => {
+    if (price) {
+    this.showPriceRangeHint = price > 1 && price < 10000;
+    }
+    });
+   }
+}
